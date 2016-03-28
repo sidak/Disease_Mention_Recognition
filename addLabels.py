d9818060de770919661a4acbceff0bc2e7ff3fd8
sidak <spssachdeva21695@gmail.com>
2016-03-28 23:58:23 +0530

    Handle spaces in disease names while searching

--- addLabels.py
+++ addLabels.py
@@ -10,10 +10,26 @@ disease_headlines_filename ="./" + sys.argv[4]
 data = pd.read_csv(input_filename)
 
 diseases = [disease.rstrip('\n') for disease in open(disease_filename, "r")]
-diseases = set(diseases)
+print diseases
 
 def diseases_in_string(str):
-    return diseases.intersection(str.split())
+	ans = []
+	for x in diseases:
+		idx = str.find(x)
+		flag = True
+		if idx == -1 :
+			flag = False
+		else:
+			if idx > 0:
+				if str[idx-1]!=' ':
+					flag = False
+			if idx < len(str):
+				if str[idx+1]!=' ':
+					flag = False
+
+		if flag:
+			ans.append(x)
+	return ans		
 
 label =[]
 
@@ -31,11 +47,11 @@ for frame in data['headline text']:
 	
 	if len(headline_diseases) > 0 :
 		label.append(1)
-		print "headline containing diseases is "
-		print headline
-		print "diseases are "
-		print headline_diseases
-		print "\n"
+		#print "headline containing diseases is "
+		#print headline
+		#print "diseases are "
+		#print headline_diseases
+		#print "\n"
 		num+= 1
 		imp_headlines.append(frame)
 	else:
