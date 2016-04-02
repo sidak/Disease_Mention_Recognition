f1eb4bdde428a09c1917665272d2b794217f3b93
ManishaPatel04 <mani.mps.04@gmail.com>
2016-04-03 01:53:28 +0530

    Adds check for 'Fight against and battle against' derivatives

---
+++ rawCheckFightAgainst.py
@@ -0,0 +1,52 @@
+import pandas as pd 
+import numpy as np 
+import nltk 
+import sys
+
+
+input_filename = './' + sys.argv[1]
+output_filename = "./" + sys.argv[2]
+
+with open(input_filename) as f:
+	content = f.readlines()
+
+output_file = open(output_filename, "w")
+
+
+for hline in content:
+	words = nltk.word_tokenize(hline)	
+	pos = nltk.pos_tag(words)
+	
+
+	flag =0 
+	idx = -1
+	for i, (key, val) in enumerate(pos):
+		if(key.lower()=='fight' or key.lower()=='battle'):
+			flag =1 
+		if(flag ==1 and key.lower()=='against' ):
+			idx = i+1
+			break
+			
+	disease_name=""
+
+	if idx!=-1:
+		pos1 = pos[idx:]
+		flag1 = 0
+		for i, (key, val) in enumerate(pos1):
+			if val == 'DT' and not flag1:
+				continue
+			elif val == 'NN' or val == 'NNP' or val == 'NNS'or val == 'NNPS'or val == 'CD' :
+				flag1 = 1;
+				if i==0:
+					disease_name+= key+" "
+				elif i>0 and (pos1[i-1][1] == 'NN' or pos1[i-1][1] == 'NNP'or pos1[i-1][1] == 'NNS' or pos1[i-1][1] == 'NNPS ' or pos1[i-1][1] == 'CD' or pos1[i-1][1] == 'JJ'):
+					disease_name+= key+" "	
+
+			elif val == 'JJ' and not flag1:
+				disease_name+= key+" "
+
+			else:
+				break
+
+		disease_name+= "\n"
+		output_file.write(disease_name)
