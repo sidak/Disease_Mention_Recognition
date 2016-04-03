c9e7a67873ee729b835c33cfb9fd45475c5b23d0
sidak <spssachdeva21695@gmail.com>
2016-04-04 02:20:26 +0530

    Add bash script to find headlines with keywords for 2015

---
+++ keywordHeadlines2015.sh
@@ -0,0 +1,18 @@
+ grep "battling" -i  data/headline_text/2015_headline_text.txt > data/keyword_headlines_2015/battling_headlines.txt
+ grep "battling with" -i  data/headline_text/2015_headline_text.txt > data/keyword_headlines_2015/battling_with_headlines.txt
+ grep "cure" -i -w data/headline_text/2015_headline_text.txt > data/keyword_headlines_2015/cure_headlines.txt
+ grep "diagnose" -i -w data/headline_text/2015_headline_text.txt > data/keyword_headlines_2015/diagnose_headlines.txt
+ grep "diagnosed by" -i  data/headline_text/2015_headline_text.txt > data/keyword_headlines_2015/diagnosed_by_headlines.txt
+ grep "diagnosed" -i  data/headline_text/2015_headline_text.txt > data/keyword_headlines_2015/diagnosed_with_for_headlines.txt
+ grep "diagnosis" -i  data/headline_text/2015_headline_text.txt > data/keyword_headlines_2015/diagnosis_headlines.txt
+ grep "diagnoses" -i  data/headline_text/2015_headline_text.txt > data/keyword_headlines_2015/diagnoses_headlines.txt
+ grep "died of" -i  data/headline_text/2015_headline_text.txt > data/keyword_headlines_2015/died_of_headlines.txt
+ grep "outbreak of" -i  data/headline_text/2015_headline_text.txt > data/keyword_headlines_2015/outbreak_of_headlines.txt
+ grep "outbreak" -i  data/headline_text/2015_headline_text.txt > data/keyword_headlines_2015/outbreak_headlines.txt
+ grep "symptom" -i  data/headline_text/2015_headline_text.txt > data/keyword_headlines_2015/symptom_headlines.txt
+ grep "vaccine" -i  data/headline_text/2015_headline_text.txt > data/keyword_headlines_2015/vaccine_headlines.txt
+ grep "treatment" -i  data/headline_text/2015_headline_text.txt > data/keyword_headlines_2015/treatment_headlines.txt
+ grep "hospital with" -i -w  data/headline_text/2015_headline_text.txt > data/keyword_headlines_2015/hospital_with_headlines.txt
+ grep "patient" -i -w data/headline_text/2015_headline_text.txt > data/keyword_headlines_2015/patient_headlines.txt
+ grep "virus" -i -w data/headline_text/2015_headline_text.txt > data/keyword_headlines_2015/virus_headlines.txt
+ grep "treatment for" -i  data/headline_text/2015_headline_text.txt > data/keyword_headlines_2015/treatment_for_headlines.txt
